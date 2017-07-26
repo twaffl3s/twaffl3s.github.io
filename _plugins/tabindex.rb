3764731782d7e4064e32488513f3fbdea5ae5f13
Antoinette Janus <acj.gfx@gmail.com>
2017-07-26 12:53:33 -0400

    Correct plugin

--- _plugins/tabindex.rb
+++ _plugins/tabindex.rb
@@ -1,7 +1,9 @@
-module TabIndex
-  def tab_index(post_index)
-    # we add 9 because of the links in the nav
-    post_index + 9
+module Jekyll
+  module TabIndex
+    def tab_index(post_index)
+      # we add 9 because of the links in the nav
+      post_index + 9
+    end
   end
 end
 
