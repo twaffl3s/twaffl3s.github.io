df17c57b376f4f50dbc701af9d6b89e48d4467ed
Antoinette Janus <acj.gfx@gmail.com>
2017-07-26 12:52:11 -0400

    Attempt for module

--- _plugins/tabindex.rb
+++ _plugins/tabindex.rb
@@ -1,9 +1,7 @@
-module Jekyll
-  module TabIndex
-    def tab_index(post_index)
-      # we add 9 because of the links in the nav
-      post_index + 9
-    end
+module TabIndex
+  def tab_index(post_index)
+    # we add 9 because of the links in the nav
+    post_index + 9
   end
 end
 
