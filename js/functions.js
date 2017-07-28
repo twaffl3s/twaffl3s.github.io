9f378659aafe34267a699a970170db42317e7463
Antoinette C. Janus <acj.gfx@gmail.com>
2017-07-28 10:41:04 -0400

    Added error catch

--- js/functions.js
+++ js/functions.js
@@ -22,5 +22,9 @@ document.addEventListener('DOMContentLoaded', function (e) {
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register('../sw.js')
-		.then(function () { console.log("Service Worker Registered"); });
+		.then(function () {
+			console.log("Service Worker Registered");
+		}, function (err) {
+			console.error(err);
+		});
 }
