c92fd1bc914c9fc96409423a6c377a69ced01b84
Antoinette C. Janus <acj.gfx@gmail.com>
2017-07-28 10:33:45 -0400

    disabling service worker for now

--- js/functions.js
+++ js/functions.js
@@ -19,8 +19,8 @@ document.addEventListener('DOMContentLoaded', function (e) {
 });
 
 // registering service worker
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('../sw.js')
-		.then(function () { console.log("Service Worker Registered"); });
-}
+// if ('serviceWorker' in navigator) {
+// 	navigator.serviceWorker
+// 		.register('../sw.js')
+// 		.then(function () { console.log("Service Worker Registered"); });
+// }
